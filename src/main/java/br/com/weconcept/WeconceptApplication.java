582314ab94c4136016907868b1f527fcb18e53ba
Fausto neris <fausto2102@hotmail.com>
2025-04-24 13:07:39 -0300

    first commit

---
+++ src/main/java/br/com/weconcept/WeconceptApplication.java
@@ -0,0 +1,13 @@
+package br.com.weconcept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeconceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeconceptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/weconcept/WeconceptApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.weconcept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeconceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
